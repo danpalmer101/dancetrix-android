f4dd9e3f4a38bc7097e8739b2e4616b4179808fa
Daniel Palmer <d.palmer101@googlemail.com>
2017-11-01 17:06:22 +0000

    Added home menu options, implemented visit website button, and set min SDK version to 21 so AppCompat is always available

--- app/src/main/java/uk/co/dancetrix/HomeActivity.java
+++ app/src/main/java/uk/co/dancetrix/HomeActivity.java
@@ -1,7 +1,11 @@
 package uk.co.dancetrix;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Toolbar myToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
     }
+
+    public void visitWebsite(View view) {
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(BuildConfig.WEBSITE_URL));
+        startActivity(i);
+    }
+
 }
