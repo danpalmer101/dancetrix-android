85f19c16dcbc8b75fa76e1bb300c128f714c258c
Daniel Palmer <d.palmer101@googlemail.com>
2017-11-02 09:18:58 +0000

    Set logo on home activity toolbar

--- app/src/main/java/uk/co/dancetrix/HomeActivity.java
+++ app/src/main/java/uk/co/dancetrix/HomeActivity.java
@@ -15,6 +15,8 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Toolbar myToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 
     public void visitWebsite(View view) {
