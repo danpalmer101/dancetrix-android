0f520188e78a55a8acd04006931a6ea7ef1ad28a
Daniel Palmer <d.palmer101@googlemail.com>
2017-11-24 11:49:23 +0000

    Added payment page text and 'let us know' button

--- app/src/main/java/uk/co/dancetrix/activity/PaymentActivity.java
+++ app/src/main/java/uk/co/dancetrix/activity/PaymentActivity.java
@@ -1,9 +1,16 @@
 package uk.co.dancetrix.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.TextView;
 
 import uk.co.dancetrix.R;
+import uk.co.dancetrix.util.Configuration;
 
 public class PaymentActivity extends AppCompatActivity {
 
@@ -11,6 +18,14 @@ public class PaymentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+
+        TextView tv = findViewById(R.id.paymentInfoView);
+        tv.setText(Html.fromHtml(getString(R.string.payment_info_html)));
+        tv.setMovementMethod(new ScrollingMovementMethod());
+    }
+
+    public void displayPaymentLetUsKnow(View view) {
+        // TODO
     }
 
 }
